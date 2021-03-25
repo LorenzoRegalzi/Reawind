2cb78090b6198e9b5faf139dd3682594aec9183a
LorenzoRegalzi <lorenzo.regalzi@gmail.com>
2021-03-25 21:55:50 +0100

    /* Add Material Grid */

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import './App.css';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: 60
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: 400
+  },
+}));
+
+export default function FullWidthGrid() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={8}>
+          <Paper className={classes.paper}>city selected</Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper}>Select city</Paper>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Paper className={classes.paper}>today weather</Paper>
+        </Grid>
+        <Grid item xs={6} sm={5}>
+          <Paper className={classes.paper}>month and week weather</Paper>
+        </Grid>
+        <Grid item xs={6} sm={4}>
+          <Paper className={classes.paper}>searching</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
-
-export default App;
